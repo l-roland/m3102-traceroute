86d8f1916c31ed4f719f4fd1e80a8608fbc5a25d
Louis Roland <66991422+l-roland@users.noreply.github.com>
2020-10-16 16:30:38 +0200

    add comments

--- graphe.sh
+++ graphe.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#ROLAND Louis - R&T2 A2
 
+#delete previous <.dot> file
 rm -rf graphe.dot
 
+#if not args
 if_rte=${1:?"Veuillez indiquer au moins un fichier .rte"}
 
 echo "--------------------------------------------"
@@ -11,28 +14,54 @@ echo "./graphe.sh [.rte]"
 echo "Tous les fichiers .rte présents dans le répertoire courant seront utilisés pour créer le graphique"
 echo "--------------------------------------------"
 
-nb=`ls -l | grep .rte | wc -l`
-echo "$nb fichier .rte présents"
-a=1
-
+#start graph
 echo "digraph graphe {" >> graphe.dot
+
+#create graph .rte per .rte
 for file in $@
 do
+	#set line1 varaible to 1
+	line1=1
+	
+	#generate radom color for arrows (1 color per .rte)
 	declare -a color=($(hexdump -n 3 -v -e '"#" 3/1 "%02X" "\n"' /dev/urandom))
+	
+	#print terminal <file>.rte used
 	echo "OK - $file"
+	
+	#extract address from file name
 	server=`echo $file | sed 's/.rte//g'`
-	len=$(wc -l $file|cut -d " " -f 1)
-	while [ $a -lt $len ]
+	
+	#set length of the file
+	len=$(wc -l $file|cut -d" " -f1)
+	
+	#write .dot while addr1 < file length
+	while [ $line1 -lt $len ]
 	do
-		b=$(($a + 1))
-		src=$(cat $file|head -n $a|tail -n 1)
-		dst=$(cat $file|head -n $b|tail -n 1)
-		((a++))
+		#set line2 varaible to line1+1
+		line2=$(($line1 + 1))
+		
+		#extract ip source in terms of line1
+		src=$(cat $file|head -n$line1|tail -n1)
+		
+		#extract ip destination in terms of line2
+		dst=$(cat $file|head -n$line2|tail -n1)
+		
+		#increment line1 of 1
+		((line1++))
+		
+		#print source -> destination to graphe
 		echo "\"$src\"->\"$dst\"[color=\"${color}\"]"  ";" >> graphe.dot
 	done
+	
+	#when file finished, print destination -> final address
 	echo "\"$dst\"->\"$server\"[arrowhead=none,penwidth=2]" >> graphe.dot
-	a=1
+	
+	#set line1 to 1
+	line1=1
 done
+
+#end of the graph
 echo "}" >> graphe.dot
 
 echo "--------------------------------------------"
